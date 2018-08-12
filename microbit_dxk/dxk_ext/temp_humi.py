52f602d5d0f6f01d4a6fcc91e086b1c79b6b4433
Yukkuri C <799433638@qq.com>
2018-08-12 17:00:01 +0800

    修改细节

--- microbit_dxk/dxk_ext/temp_humi.py
+++ microbit_dxk/dxk_ext/temp_humi.py
@@ -1,4 +1,5 @@
 from mb import *
+_res=[]
 def temp(addr=None):
   return command(slot(addr,2),b'get_temp',1)
 def humi(addr=None):
@@ -6,7 +7,14 @@ def humi(addr=None):
 def temp_humi(addr=None):
   res=command(slot(addr,2),b'get_temp_humi',2)
   if isinstance(res,int):
-    return res//256,res%256
+    t,h=res//256,res%256
+    if t>=128:t-=256
+    return t,h
   if res==None:
     return None,None
-  return [(r//256,r%256) for r in res]
+  _res.clear()
+  for rr in res:
+    t,h=rr//256,rr%256
+    if t>=128:t-=256
+    _res.append((t,h))
+  return tuple(_res)
