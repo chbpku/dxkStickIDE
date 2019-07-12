a980960515c49b2b0d71e33d086b4ce088587889
ChenBin <chbpku@gmail.com>
2019-07-12 14:54:18 +0800

    新增初始化py文件。

--- Lessons/HiDXK.py
+++ Lessons/HiDXK.py
@@ -3,6 +3,6 @@ from microbit import *
 display.scroll("Hi!DXK")
 while True:
     display.show(Image.HEART)
-    sleep(100)
+    sleep(600)
     display.show(Image.HEART_SMALL)
-    sleep(100)
+    sleep(200)
